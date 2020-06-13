db26e36f600c29caf22e15b34197d1287a068a60
zhangwanli-marker <18321228089@163.com>
2020-06-14 06:57:10 +0800

    yaml1

--- traning/test_demo1.py
+++ traning/test_demo1.py
@@ -6,7 +6,7 @@ class TestData:
     # with open('listdata.yaml', 'rb') as f:
     #     data = yaml.safe_load(f)
 
-    @pytest.mark.parametrize(['a', 'b'], yaml.safe_load(open("listdata.yaml")))
+    @pytest.mark.parametrize("a, b", yaml.safe_load(open("./listdata.yaml")))
     def test_data(self, a, b):
         c = a + b
         print(c)
