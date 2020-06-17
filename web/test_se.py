82821e36aff0adc2576c51463cdbe3be2dac07f7
zhangwanli-marker <18321228089@163.com>
2020-06-17 19:29:10 +0800

    commit2

--- web/test_se.py
+++ web/test_se.py
@@ -2,6 +2,8 @@ from time import sleep
 
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -9,20 +11,18 @@ class Test_wanli:
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.implicitly_wait(1)
+        self.driver.implicitly_wait(3)
         self.driver.get("https://home.testing-studio.com")
         sleep(1)
 
     def teardown(self):
         self.driver.quit()
 
-    def test_Hogwos(self):
-        self.driver.find_element(By.XPATH,'//*[@title="sd sd "]')
-
-        def wait(x):
-            return len(self.driver.find_elements(by=self.driver.)) >= 1
-
-        WebDriverWait(self.driver, 3).until(wait)
-
-
-a sdfsf cs 
+    def test_wait(self):
+        self.driver.find_element(By.XPATH, '//*[@title="归入各种类别的所有主题"]').click()
+        #
+        # def wait(x):
+        #     return len(self.driver.find_elements(By.XPATH, '//*[@class="table-heading"]')) >= 1
+        expected_conditions.element_to_be_clickable
+        WebDriverWait(self.driver, 3).until(expected_conditions((By.XPATH, '//*[@class="table-heading"]')))
+        self.driver.find_element(By.XPATH, '//*[@title="在最近的一年，一月，一周或一天最活跃的主题"]')
